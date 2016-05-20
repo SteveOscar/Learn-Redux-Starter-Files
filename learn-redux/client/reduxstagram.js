266440afc26492a801d334e6d8205d6d95881ed8
Steve Olson <steveoscaro@gmail.com>
2016-05-20 14:39:30 -0600

    project setup

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,11 @@
-// let's go!
+import React from 'react'
+
+import { render } from 'react-dom'
+
+//Import css
+import css from './styles/style.styl'
+
+//Import Componenets
+import Main from './componenets/Main'
+
+render(<Main/>, document.getElementById('root'))
