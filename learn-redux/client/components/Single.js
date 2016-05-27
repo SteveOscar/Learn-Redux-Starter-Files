c6c7e1b717d8ec9feb6b96eb7aabbfe9d9697ba6
Steve Olson <steveoscaro@gmail.com>
2016-05-27 12:41:56 -0600

    add comment functionality

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import Photo from './Photo'
+import Comments from './Comments'
 // import comments
 
 const Single = React.createClass({
   render() {
-      //index of the post
-      const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId)
+      const { postId } = this.props.params
+      const i = this.props.posts.findIndex((post) => post.code === postId)
       //get the post
       const post = this.props.posts[i]
-      {debugger}
+      const postComments = this.props.comments[postId] || []
       return(
         <div className="single-photo">
           <Photo i={i} post={post} {...this.props} />
+          <Comments postComments={postComments}/>
         </div>
     )
   }
